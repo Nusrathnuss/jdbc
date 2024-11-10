ccc8d55e3de1cff7dc775fee3b82ce995e727db3
Nusrathnuss <140223312+Nusrathnuss@users.noreply.github.com>
2024-11-10 10:36:54 +0530

    Add files via upload
    Blob for inserting images

---
+++ writeImage.java
@@ -0,0 +1,48 @@
+package bankdetails;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class writeImage {
+	private static String url="jdbc:mysql://localhost:3306/emplyee";
+	private static String un="root";
+	private static String pw="root";
+	static Connection con;
+	private static String sql="update employee set photo=? where id=?";
+	private static PreparedStatement pstmt;
+	private static FileInputStream fis;
+
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con=DriverManager.getConnection(url,un,pw);
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		try {
+			
+			pstmt=con.prepareStatement(sql);
+			System.out.println("enter the id");
+			//to avoid memory wastage we can also write like this that creting object and simultaneously collecting integer data
+			//int id=new Scanner(System.in).nextInt();
+			//instead of writing like this just write where ever u need it to avoid extra usage of memory
+			pstmt.setInt(2, new Scanner(System.in).nextInt());
+			//create obj of name fileinpstream
+			//here double slash is one it will take escape so
+			fis=new FileInputStream("F:\\imagess\\tim.png");
+			pstmt.setBinaryStream(1, fis);
+			int x=pstmt.executeUpdate();
+			System.out.println(x+"rows affected");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}	// TODO Auto-generated method stub
+	}
+}
